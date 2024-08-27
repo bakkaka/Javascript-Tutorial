eb763aa83d0004a9b313c19f120edfa441cf0b6c
bakkaka <abdoubakka@gmail.com>
2024-08-27 13:30:39 +0100

    My first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let gameDiv = document.getElementById("gameDiv")
+console.log(gameDiv)
+let gameDivH2 = document.querySelector("#gameDiv h2")
+console.log(gameDivH2)
+let allH2 = document.querySelectorAll("h2")
+console.log(allH2)
+for (let i = 0; i < allH2.length; i++){
+    console.log(allH2[i])
+
+}
+    
